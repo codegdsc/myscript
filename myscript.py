9bcc0e40a95f803638c1c5ebff3ee43abf0f052f
codegdsc <riddhibhagat01@gmail.com>
2023-10-11 12:39:49 +0530

    created new project

---
+++ myscript.py
@@ -0,0 +1,7 @@
+message="you are so beautiful. I love you"
+print(message)
+reply=input("send your reply: ")
+if reply=="yes":
+    print("sapne me dekha tha jisko ajj wahi din aaya hai")
+else:
+    print("Thukra ke mera pyar mera intekam dekhegi")
